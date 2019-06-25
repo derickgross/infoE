2c994ced68f315ec5308c509bb34c369744b9e87
Derick Gross <derick.gross@gmail.com>
2019-06-25 19:13:44 -0400

    merge sort listings returned from getFavorites

--- src/FavoriteListings.js
+++ src/FavoriteListings.js
@@ -18,13 +18,40 @@ FavoriteListings.prototype.addListing = function(listing) {
 };
 
 FavoriteListings.prototype.removeListing = function(listing) {
-	if (!!this.listings) {
+	if (this.listings.includes(listing)) {
 		this.listings = this.listings.filter(x => x.listings_id !== listing.listings_id)
 	}
 };
 
 FavoriteListings.prototype.getFavorites = function() {
-  return this.listings;
+	console.log(`first listing num_favorers: ${!!this.listings[0] ? this.listings[0].num_favorers : 'no listings'}`);
+  return mergeSort(this.listings);
+  //return this.listings
+}
+
+function mergeSort(array) {
+  if (array.length > 1) {
+    const arrayRight = array.splice(array.length/2);
+
+    const sortedListings =  mergeListingArrays(mergeSort(array), mergeSort(arrayRight));
+
+    console.log(`sorted listings num_favorers: ${sortedListings.map(listing => listing.num_favorers)}`);
+
+    return sortedListings;
+  }
+  else {
+    return array;
+  }
+}
+
+function mergeListingArrays(first, second) {
+  const result = [];
+
+  while (!!first.length && !!second.length) {
+    first[0].num_favorers >= second[0].num_favorers ? result.push(first.shift()) : result.push(second.shift())
+  }
+
+  return result.concat(first, second);
 }
 
 module.exports = FavoriteListings;
